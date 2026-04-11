88d2bda99289aa5148d361d41f4769fdce4d3374
RaheelHusain <raheelhussain3000@gmail.com>
2026-04-12 04:03:12 +0500

    fixed cors for deployment

--- app.js
+++ app.js
@@ -1,31 +1,68 @@
+// require('dotenv').config();
+
+
+// const express = require('express');
+// const cors = require('cors')
+// const connectToDb = require('./db/database');
+// const userRoutes = require('./routes/user.routes');
+// const cookieparser = require('cookie-parser')
+// const captainRoutes = require("./routes/captain.routes")
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }));
+// app.use(cookieparser());
+
+// connectToDb();
+
+
+// app.get('/', (req,res) => {
+//     res.send('Hello World')
+// })
+// app.use('/users', userRoutes);
+// app.use('/captain', captainRoutes);
+
+
+// module.exports = app;
+
+
 require('dotenv').config();
 
-
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors'); // CORS package include kiya
 const connectToDb = require('./db/database');
 const userRoutes = require('./routes/user.routes');
-const cookieparser = require('cookie-parser')
-const captainRoutes = require("./routes/captain.routes")
+const cookieparser = require('cookie-parser');
+const captainRoutes = require("./routes/captain.routes");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
-app.use(cookieparser());
-
+// Database connection
 connectToDb();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
+// CORS Setup: Is se aapka frontend backend se connect ho payega
+app.use(cors({
+    origin: '*', // '*' ka matlab hai ke abhi kisi bhi URL se request accept hogi (deployment ke liye best hai)
+    credentials: true
+}));
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('Hello World - NEXride Backend is Live!');
+});
+
+// API Routes
 app.use('/users', userRoutes);
 app.use('/captain', captainRoutes);
-
 
 module.exports = app;
