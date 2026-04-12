9231cccffc470de154718fcfd21c99c1f240012a
RaheelHusain <raheelhussain3000@gmail.com>
2026-04-12 06:41:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -31,10 +31,47 @@
 // module.exports = app;
 
 
-require('dotenv').config();
+// require('dotenv').config();
 
+// const express = require('express');
+// const cors = require('cors'); // CORS package include kiya
+// const connectToDb = require('./db/database');
+// const userRoutes = require('./routes/user.routes');
+// const cookieparser = require('cookie-parser');
+// const captainRoutes = require("./routes/captain.routes");
+
+// const app = express();
+
+// // Database connection
+// connectToDb();
+
+// // Middlewares
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieparser());
+
+// // CORS Setup: Is se aapka frontend backend se connect ho payega
+// app.use(cors({
+//     origin: 'https://ne-xride-frontend.vercel.app', // '*' ka matlab hai ke abhi kisi bhi URL se request accept hogi (deployment ke liye best hai)
+//     credentials: true
+// }));
+
+// // Basic Route
+// app.get('/', (req, res) => {
+//     res.send('Hello World - NEXride Backend is Live!');
+// });
+
+// // API Routes
+// app.use('/users', userRoutes);
+// app.use('/captain', captainRoutes);
+
+// module.exports = app;
+
+
+
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); // CORS package include kiya
+const cors = require('cors');
 const connectToDb = require('./db/database');
 const userRoutes = require('./routes/user.routes');
 const cookieparser = require('cookie-parser');
@@ -42,19 +79,19 @@ const captainRoutes = require("./routes/captain.routes");
 
 const app = express();
 
-// Database connection
-connectToDb();
+// SABSE PEHLE CORS
+app.use(cors({
+    origin: 'https://ne-xride-frontend.vercel.app',
+    credentials: true
+}));
 
-// Middlewares
+// Phir baaki middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 
-// CORS Setup: Is se aapka frontend backend se connect ho payega
-app.use(cors({
-    origin: 'https://ne-xride-frontend.vercel.app', // '*' ka matlab hai ke abhi kisi bhi URL se request accept hogi (deployment ke liye best hai)
-    credentials: true
-}));
+// Database connection
+connectToDb();
 
 // Basic Route
 app.get('/', (req, res) => {
